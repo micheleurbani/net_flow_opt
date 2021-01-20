cd83ed30430ac930a44566de6f6f12c7ff3ea779
Michele Urbani <michele@didatec.com>
2021-01-20 11:36:19 +0100

    Added penalty function for deferment of activities.

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,11 +1,44 @@
 class Activity(object):
+    """
+    The object desribes a maintenance activity; this is always associated to a
+    group when the activity is added to a plan.
+
+    :param object component: an instance of :class:`core.system.Component`
+    object.
+    :param float date: the due date of the activity.
+    :param floa duration: the duration of the activity.
+
+    """
 
     def __init__(self, component, date, duration):
         self.component = component
         self.t = date
         self.d = duration
-        self.t_opt = None
+        self.t_opt = self.t
 
+    def expectedCost(self, x):
+        return self.component.cp + self.component.cc * \
+            (x / self.component.alpha) ** self.component.beta
+
+    def h(self, delta_t):
+        """Penalty function to defer the component from its PM date."""
+        return self.expectedCost(self.component.xStar + delta_t) - \
+            self.expectedCost(self.component.xStar) - delta_t * \
+                self.component.phiStar
+
+    def dh(self, delta_t):
+        """Derivative of the penalty function."""
+        return self.component.cc * self.component.alpha ** \
+            (-self.component.beta) * (self.component.xStar + delta_t) ** \
+            (self.component.beta - 1) * self.compoennt.beta - \
+                self.component.phiStar
+
+    def ddh(self, delta_t):
+        """Second derivative of the penalty function."""
+        return self.component.cc * self.component.alpha ** \
+            (-self.component.beta) * self.component.beta * \
+            (self.component.beta - 1) * (self.component.xStar + delta_t) \
+                ** (self.component.beta - 2)
 
 class Group(object):
 
