ce716b92fd02ee4ced6215f4b0d85e25728d6f71
Michele Urbani <michele@didatec.com>
2021-01-22 15:27:08 +0100

    Renamed tests and added assignment to resources.

--- core/tests.py
+++ core/tests.py
@@ -183,6 +183,7 @@ class PlanTestCase(unittest.TestCase):
         # Change the plan by adding a random grouping structure
         self.add_random_grouping()
         for a in self.activities:
+            print(a.r)
             self.assertIsNotNone(a.r)
 
     def test_generate_structure_history(self):
