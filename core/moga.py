d754864356c6507734eb1cd4d53e1b6e519b945f
Michele Urbani <michele@didatec.com>
2021-02-10 12:02:58 +0100

    Fixex HV: only ind. with r=1.

--- core/moga.py
+++ core/moga.py
@@ -526,7 +526,6 @@ class MOGAResults(object):
 
     @staticmethod
     def generation_HV(n_sample, list_of_scores, bounds):
-        print(list_of_scores)
         lf_max, lf_min, ic_max, ic_min = bounds
         dominated = 0
         for _ in range(n_sample):
@@ -564,8 +563,8 @@ class MOGAResults(object):
 
         # Retrieve the list of scores
         scores = [
-            [(ind.plan.LF, ind.plan.IC) for ind in generation] for generation
-            in self.moga.population_history
+            [(ind.plan.LF, ind.plan.IC) for ind in generation if ind.rank==1]
+            for generation in self.moga.population_history
         ]
         # Wrap bounds in a tuple
         bounds = (lf_max, lf_min, ic_max, ic_min)
